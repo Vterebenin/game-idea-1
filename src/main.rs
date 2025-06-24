869a4106d5f48e7db13a95352f4702e26031a390
Valentin Terebenin <terebenin.work@gmail.com>
2025-06-25 00:00:25 +0300

    feat: initialized

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use avian3d::prelude::*;
+use bevy::{prelude::*, scene::SceneInstanceReady};
+use bevy_skein::SkeinPlugin;
+
+fn on_add_test_subject(trigger: Trigger<OnAdd, TestSubject>, mut commands: Commands) {
+    commands
+        .entity(trigger.target())
+        .insert((Collider::cylinder(4.0, 0.1), RigidBody::Static));
+}
+
+fn on_add_character(trigger: Trigger<OnAdd, Character>, mut commands: Commands) {
+    commands.entity(trigger.target()).insert((
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
+    ));
+}
+
+fn main() {
+    App::new()
+        .register_type::<Character>()
+        .register_type::<TestSubject>()
+        .add_plugins((
+            DefaultPlugins,
+            SkeinPlugin::default(),
+            PhysicsPlugins::default(),
+            PhysicsDebugPlugin::default(),
+        ))
+        .add_observer(on_add_test_subject)
+        .add_observer(on_add_character)
+        .add_observer(
+            // log the component from the gltf spawn
+            |trigger: Trigger<SceneInstanceReady>,
+             children: Query<&Children>,
+             characters: Query<&Character>| {
+                for entity in children.iter_descendants(trigger.target()) {
+                    let Ok(character) = characters.get(entity) else {
+                        continue;
+                    };
+                    info!(?character);
+                }
+            },
+        )
+        .add_systems(Startup, startup)
+        .run();
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct Character {
+    name: String,
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct TestSubject;
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(SceneRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("cars/car1.gltf")),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+    // Static physics object with a collision shape
+    // commands.spawn((
+    //     RigidBody::Static,
+    //     Collider::cylinder(4.0, 0.1),
+    //     Mesh3d(meshes.add(Cylinder::new(4.0, 0.1))),
+    //     MeshMaterial3d(materials.add(Color::WHITE)),
+    // ));
+
+    // // Dynamic physics object with a collision shape and initial angular velocity
+    // commands.spawn((
+    //     Mesh3d(meshes.add(Cuboid::from_length(1.0))),
+    //     MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+    //     Transform::from_xyz(0.0, 4.0, 0.0),
+    // ));
+
+    // // Light
+    // commands.spawn((
+    //     PointLight {
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     Transform::from_xyz(4.0, 8.0, 4.0),
+    // ));
+
+    // // Camera
+    // commands.spawn((
+    //     Camera3d::default(),
+    //     Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
+    // ));
+}
