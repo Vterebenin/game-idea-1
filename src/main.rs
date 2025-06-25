526531fb3f60e5ae61fd64e1acaa07da9e55dd61
Valentin Terebenin <terebenin.work@gmail.com>
2025-06-26 02:07:44 +0300

    chore: cars

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use avian3d::prelude::*;
-use bevy::{gltf::GltfMesh, prelude::*, scene::SceneInstanceReady};
+use bevy::{prelude::*, scene::SceneInstanceReady};
 use bevy_skein::SkeinPlugin;
-
-fn on_add_character(trigger: Trigger<OnAdd, Character>, mut commands: Commands) {
-    commands.entity(trigger.target()).insert((
-        RigidBody::Dynamic,
-        Collider::cuboid(1.0, 1.0, 1.0),
-        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
-    ));
-}
 
 fn main() {
     App::new()
@@ -20,7 +12,6 @@ fn main() {
             PhysicsPlugins::default(),
             PhysicsDebugPlugin::default(),
         ))
-        .add_observer(on_add_character)
         .add_observer(
             // log the component from the gltf spawn
             |trigger: Trigger<SceneInstanceReady>,
@@ -51,16 +42,9 @@ struct AutoCollider;
 fn startup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn(SceneRoot(
         asset_server.load(GltfAssetLabel::Scene(0).from_asset("cars/car1.gltf")),
-    ));
-    commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     ));
     commands.spawn((
         PointLight {
